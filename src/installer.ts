794c253aa620f9eb640ea5bd5d8d71c717ff44e3
jared-w <jaredweakly@gmail.com>
2019-11-17 09:02:49 -0800

    Rough first draft of bash->js translation

--- src/installer.ts
+++ src/installer.ts
@@ -1,38 +1,146 @@
 import * as core from '@actions/core';
+import * as io from '@actions/io';
+import {exec} from '@actions/exec';
 import * as fs from 'fs';
+import * as os from 'os';
 import * as path from 'path';
+import {fetch} from 'cross-fetch';
 
-export function findHaskellGHCVersion(baseInstallDir: string, version: string) {
-  return _findHaskellToolVersion(baseInstallDir, 'ghc', version);
+async function ex(cmd: string, ...args: string[]): Promise<void> {
+  let err = '';
+  if (!(await exec(cmd, args, {listeners: {stdout: d => (err += `${d}`)}}))) {
+    core.error(err);
+    throw new Error(`command ${args.join(' ')} failed`);
+  }
 }
 
-export function findHaskellCabalVersion(
-  baseInstallDir: string,
-  version: string
-) {
-  return _findHaskellToolVersion(baseInstallDir, 'cabal', version);
+async function linuxGHC(version: string): Promise<void> {
+  if (!fs.existsSync(`/opt/ghc/${version}/bin`)) {
+    // This assumes the existance of the GHC PPA `ppa:hvr/ghc`
+    await ex('sh', 'sudo', 'apt-get', 'install', '-y', `ghc-${version}`);
+  }
+
+  core.addPath(path.join('opt', 'cabal', '3.0', 'bin'));
+  core.addPath(path.join('opt', 'ghc', version, 'bin'));
+
+  [
+    `::set-output name=store-dir::${os.homedir()}/.cabal/store`,
+    '::add-path::/opt/cabal/3.0/bin',
+    '::add-path::/opt/ghc/$GHCVER/bin'
+  ].forEach(core.info);
 }
 
-export function _findHaskellToolVersion(
-  baseInstallDir: string,
-  tool: string,
-  version: string
-) {
-  if (!baseInstallDir) {
-    throw new Error('baseInstallDir parameter is required');
-  }
-  if (!tool) {
-    throw new Error('toolName parameter is required');
-  }
-  if (!version) {
-    throw new Error('versionSpec parameter is required');
-  }
+async function macGHC(version: string): Promise<void> {
+  const cabalTar = path.join(os.tmpdir(), 'cabal.tar.xz');
+  const cabalBin = path.join('opt', 'cabal', '3.0', 'bin');
+  await io.mkdirP(cabalBin);
+  fs.writeFileSync(
+    cabalTar,
+    await fetch(
+      'https://downloads.haskell.org/cabal/cabal-install-3.0.0.0/cabal-install-3.0.0.0-x86_64-apple-darwin17.7.0.tar.xz'
+    ).then(r => r.blob())
+  );
+  await ex('sh', 'sudo', 'tar', '-xvJf', cabalTar, '-C', cabalBin);
+  fs.unlinkSync(path.join(cabalTar));
 
-  const toolPath: string = path.join(baseInstallDir, tool, version, 'bin');
-  if (fs.existsSync(toolPath)) {
-    core.debug(`Found tool in cache ${tool} ${version}`);
-    core.addPath(toolPath);
-  } else {
-    throw new Error(`Version ${version} of ${tool} not found`);
+  await io.mkdirP(path.join('opt', 'ghc'));
+  await exec('sh', ['sudo', 'chown', 'runner:wheel', path.join('opt', 'ghc')]);
+
+  const ghcURL = {
+    '7.10.3':
+      'https://downloads.haskell.org/ghc/7.10.3/ghc-7.10.3b-x86_64-apple-darwin.tar.xz ',
+    '8.0.2':
+      'https://downloads.haskell.org/~ghc/8.0.2/ghc-8.0.2-x86_64-apple-darwin.tar.xz ',
+    '8.2.2':
+      'https://downloads.haskell.org/~ghc/8.2.2/ghc-8.2.2-x86_64-apple-darwin.tar.xz ',
+    '8.4.4':
+      'https://downloads.haskell.org/~ghc/8.4.4/ghc-8.4.4-x86_64-apple-darwin.tar.xz ',
+    '8.6.5':
+      'https://downloads.haskell.org/~ghc/8.6.5/ghc-8.6.5-x86_64-apple-darwin.tar.xz '
+  }[version as '7.10.3'];
+
+  const ghcFile = path.join(os.tmpdir(), 'ghc.tar.xz');
+  fs.writeFileSync(ghcFile, await fetch(ghcURL).then(r => r.blob()));
+  if (!fs.existsSync(ghcFile)) {
+    throw new Error('ghc download failed');
+  }
+  const ghcInst = path.join(os.tmpdir(), 'ghcinst');
+  const ghcVer = path.join('opt', 'ghc', version);
+  io.mkdirP(ghcInst);
+  await exec('sh', ['tar', '-xJf', ghcFile, '-C', ghcInst]);
+  fs.unlinkSync(ghcFile);
+  await ex('sh', path.join(ghcInst, 'configure'), '--prefix', ghcVer);
+  await ex('sh', 'make', '-C', ghcInst, 'install');
+  fs.rmdirSync(ghcInst);
+
+  core.addPath(cabalBin);
+  core.addPath(path.join(ghcVer, 'bin'));
+
+  [
+    `::set-output name=store-dir::${os.homedir()}/.cabal/store`,
+    '::add-path::/opt/cabal/3.0/bin',
+    '::add-path::/opt/ghc/$GHCVER/bin'
+  ].forEach(core.info);
+}
+
+async function windowsGHC(version: string): Promise<void> {
+  const msys = path.join('c', 'tools', 'msys64', 'mingw64', 'lib');
+  io.mkdirP(msys);
+  await ex('choco', 'install', '-r', '-y', 'cabal', '--version', '3.0.0.0');
+  await ex('cabal', 'user-config', 'diff');
+  await ex(
+    'cabal',
+    'user-config',
+    'update',
+    '-a',
+    `"store-dir: ${path.join('C:\\', 'SR')}"`,
+    '-v3'
+  );
+
+  const versionFixes = {
+    '8.0.2': '8.0.2.2',
+    '7.10.3': '7.10.3.2',
+    '7.8.4': '7.8.4.1',
+    '7.6.3': '7.6.3.1'
+  };
+  const ghcVer =
+    version in versionFixes
+      ? versionFixes[version as keyof typeof versionFixes]
+      : version;
+
+  await ex('choco', 'install', '-r', '-y', 'ghc', `--version=${ghcVer}`);
+  const ghcPath = path.join(
+    'C:\\',
+    'ProgramData',
+    'chocolatey',
+    'lib',
+    'ghc',
+    'tools',
+    `ghc-${ghcVer}`,
+    'bin'
+  );
+  core.addPath(ghcPath);
+
+  [
+    '::set-env name=CABAL_DIR::C:\\CABAL',
+    '::set-output name=store-dir::C:\\SR',
+    `::add-path::${ghcPath}`
+  ].forEach(core.info);
+}
+
+type OS = 'Linux' | 'macOS' | 'Windows';
+export async function installGHC(version: string): Promise<void> {
+  const RUNNEROS = (process.env.RUNNEROS as unknown) as OS;
+  switch (RUNNEROS) {
+    case 'Linux':
+      await linuxGHC(version);
+      break;
+
+    case 'macOS':
+      await macGHC(version);
+      break;
+
+    case 'Windows':
+      await windowsGHC(version);
   }
 }
--- src/setup-haskell.ts
+++ src/setup-haskell.ts
@@ -1,25 +1,22 @@
 import * as core from '@actions/core';
-import {findHaskellGHCVersion, findHaskellCabalVersion} from './installer';
+import {installGHC} from './installer';
 
 // ghc and cabal are installed directly to /opt so use that directlly instead of
 // copying over to the toolcache dir.
-const baseInstallDir = '/opt';
+// const baseInstallDir = '/opt';
 const defaultGHCVersion = '8.6.5';
-const defaultCabalVersion = '3.0';
+// const defaultCabalVersion = '3.0';
 
-async function run() {
+async function run(): Promise<void> {
   try {
-    let ghcVersion = core.getInput('ghc-version');
-    if (!ghcVersion) {
-      ghcVersion = defaultGHCVersion;
-    }
-    findHaskellGHCVersion(baseInstallDir, ghcVersion);
+    const ghcVersion = core.getInput('ghc-version') || defaultGHCVersion;
+    await installGHC(ghcVersion);
 
-    let cabalVersion = core.getInput('cabal-version');
-    if (!cabalVersion) {
-      cabalVersion = defaultCabalVersion;
-    }
-    findHaskellCabalVersion(baseInstallDir, cabalVersion);
+    // let cabalVersion = core.getInput('cabal-version');
+    // if (!cabalVersion) {
+    //   cabalVersion = defaultCabalVersion;
+    // }
+    // findHaskellCabalVersion(baseInstallDir, cabalVersion);
   } catch (error) {
     core.setFailed(error.message);
   }
